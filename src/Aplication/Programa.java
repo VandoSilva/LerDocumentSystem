2ba195062270df5ea8915658be8ad93df892b37f
Vando Silva <shdzin@hotmail.com>
2020-11-29 21:09:05 -0300

    Comando fileReader

---
+++ src/Aplication/Programa.java
@@ -0,0 +1,31 @@
+package Aplication;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		String path = "c:\\Users\\Cronos Info\\eclipse-workspace\\in.txt";
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			String line = br.readLine();
+			
+			while (line != null) {
+				System.out.println(line);
+				line = br.readLine();
+			}
+			
+		}
+		
+		catch (IOException e) {
+			System.out.println("Error " + e.getMessage());
+		}
+		
+	}
+		
+	
+	
+	}
